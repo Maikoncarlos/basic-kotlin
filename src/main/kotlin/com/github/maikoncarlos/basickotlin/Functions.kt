5ec24a0d901491cade2d2a337d70d23e625cdf11
Maikoncarlos <maikonsaturno@gmail.com>
2023-10-15 20:17:51 -0300

    FUNCÇÕES COM VARARG

--- src/main/kotlin/com/github/maikoncarlos/basickotlin/Functions.kt
+++ src/main/kotlin/com/github/maikoncarlos/basickotlin/Functions.kt
@@ -22,6 +22,24 @@ fun sum(x: Int, y: Int): Int {
 //UMA FUNÇÃO DE EXRESSÃO UNICA QUE RETORNA UM INT POIS O RESULTADO DE DOIS INT SERÁ SEMPRE UM INT
 fun multiply(x: Int, y: Int) = x * y
 
+class FunctionsVararg
+
+//O vararg TRANSFORMA O ATRIBUTO EM UM VARARG
+//QUE PODE PASSAR NESTE CASO QUANTAS STRINGS QUISER DENTRO DA CHAMADA DO METODO, VIRANDO ASSIM UMA LISTA
+fun printAll(vararg messages: String) {
+    for (m in messages) println(m)
+}
+
+fun printAllWithPrefix(vararg messages: String, prefix: String) {
+    for (m in messages) println(prefix + m)
+}
+
+//NESTE CASO QUANDO VAMOS CHAMAR OUTRA FUNÇÃO DENTRO DA FUNÇÃO COM O vararg
+// DEVEMOS PASSAR COM O * NA FRENTE PARA IDENTIFICAR
+fun log(vararg entries: String) {
+    printAll(*entries)
+}
+
 fun main() {
     printMessage("Hello")
 
@@ -35,4 +53,14 @@ fun main() {
 
     println(sum(1, 2))
     println(multiply(2, 4))
+
+
+
+    printAll("Hello", "Hallo", "Salut", "Hola", "你好")
+    printAllWithPrefix(
+        "Hello", "Hallo", "Salut", "Hola", "你好",
+        prefix = "Greeting: "
+    )
+    log("Hello", "Hallo", "Salut", "Hola", "你好")
+
 }
